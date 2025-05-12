b9bf632ad229016c5bae165ae8738dfa9c2b0ea1
Rengoku <enderoffice7@gmail.com>
2025-05-12 06:46:42 +0300

    fix_2

--- cloud_go/main.go
+++ cloud_go/main.go
@@ -5,11 +5,38 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 var folder = []string{"text_file", "image", "video", "web", "exe", "other", "archive", "sound", "table"}
+
+func get_jwt(token string) string {
+	// Парсим токен с ключом "secret"
+	tokens, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		return []byte("secret"), nil
+	})
+
+	if err != nil || tokens == nil {
+		return "" // Возвращаем пустую строку при ошибке
+	}
+
+	// Проверяем валидность токена и тип claims
+	claims, ok := tokens.Claims.(jwt.MapClaims)
+	if !ok || !tokens.Valid {
+		return ""
+	}
+
+	// Безопасное извлечение поля "username"
+	username, ok := claims["username"].(string)
+	if !ok {
+		return "" // Если "username" нет или это не строка
+	}
+
+	return username
+}
 
 func create_folders(name string) {
 	for _, v := range folder {
@@ -39,7 +66,16 @@ func KafkaConsumer() {
 }
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("token")
+	if err != nil {
+		http.Redirect(w, r, "/register", http.StatusSeeOther)
+		return
+	}
+
+	name := get_jwt(cookie.Value)
+
 	file, handler, err := r.FormFile("file")
+
 	if err != nil {
 		fmt.Fprintln(w, "Error retrieving file:", err)
 		return
@@ -47,7 +83,12 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 
 	defer file.Close()
 
-	f, _ := os.Create(handler.Filename)
+	filepath := filepath.Join("upload", name, handler.Filename)
+	f, err := os.Create(filepath)
+	if err != nil {
+		fmt.Fprintln(w, "Error creating file:", err)
+		return
+	}
 	io.Copy(f, file)
 
 	http.Redirect(w, r, "http://localhost:8000/profile", http.StatusSeeOther)
